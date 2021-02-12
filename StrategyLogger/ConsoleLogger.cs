b6fa6f6cd5b1c70870ab7be45c525977ebd279b3
DmitryBahtenkov <cool.iridiy@gmail.com>
2021-02-12 09:15:26 +0300

    Start

---
+++ StrategyLogger/ConsoleLogger.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace StrategyLogger
+{
+    public class ConsoleLogger : ILogger
+    {
+        public void LogError(string message)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.Write($"{DateTime.Now} | ERROR: ");
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine(message);
+        }
+
+        public void LogInfo(string message)
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.Write($"{DateTime.Now} | INFO: ");
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine(message);
+        }
+    }
+}
---
+++ StrategyLogger/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace StrategyLogger
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var calculator = new Calculator(new ConsoleLogger());
+            Console.WriteLine("Введите первое число: ");
+            var first = int.Parse(Console.ReadLine());
+            
+            Console.WriteLine("Введите первое число: ");
+            var second = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Введите действие: ");
+            var action = Console.ReadLine();
+            switch (action)
+            {
+                case "/":
+                    calculator.CalculateDivide(first, second);
+                    break;
+            }
+        }
+    }
+}
